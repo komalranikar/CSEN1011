8389dd92cbcfa315a3bf324e20dfbdad508f14bb
Komal Rani Kar <93367596+komalranikar@users.noreply.github.com>
2021-11-19 12:37:43 +0530

    Add files via upload

---
+++ power.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	int i,j;
+	printf("enter your base = ");
+	scanf("%lf",&i);
+	printf("enter your exponent = ");
+	scanf("%lf",&j);
+	printf("your calculated power value is-- %f",pow(i,j));
+}
